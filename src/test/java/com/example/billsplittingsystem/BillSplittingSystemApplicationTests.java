69ff8d9c08e01196539e8a982567960ed7d528f1
Swaraj <swaraj@nair.com>
2024-01-07 15:23:42 +0530

    initial commit

---
+++ src/test/java/com/example/billsplittingsystem/BillSplittingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.billsplittingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillSplittingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
